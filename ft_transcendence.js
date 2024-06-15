dcbc8e1f277d95a94961f5ce5c20fe2eff5d9c06
Leopold Couturier-Otis <lcouturi@c2r2p5.42quebec.com>
2024-06-15 17:26:06 -0400

    replace a bunch of css properties with native bootstrap properties
    misc improvements everywhere

--- ft_transcendence.js
+++ ft_transcendence.js
@@ -16,7 +16,8 @@ function click()
 function closeAbout()
 {
 	document.getElementById("about-button").style.backgroundColor = "";
-	document.getElementById("about-button").style.borderColor = "";
+	document.getElementById("about-button").style.setProperty("--bs-border-color", "");
+	document.getElementById("about").style.boxShadow = "";
 	document.getElementById("about").style.transform = "translate(100vw, -50%)";
 	about_form_enabled = false;
 }
@@ -24,9 +25,7 @@ function closeAbout()
 function closeHome()
 {
 	document.getElementById("home-button").style.backgroundColor = "";
-	document.getElementById("home-button").style.borderColor = "";
-	document.getElementById("content").style.filter = "brightness(50%)";
-	document.getElementById("toolbar").style.filter = "brightness(50%)";
+	document.getElementById("home-button").style.setProperty("--bs-border-color", "");
 	document.getElementById("toolbar").style.pointerEvents = "none";
 	document.getElementById("content-body").style.pointerEvents = "none";
 	home_form_enabled = false;
@@ -35,7 +34,8 @@ function closeHome()
 function closeLogin()
 {
 	document.getElementById("login-button").style.backgroundColor = "";
-	document.getElementById("login-button").style.borderColor = "";
+	document.getElementById("login-button").style.setProperty("--bs-border-color", "");
+	document.getElementById("login").style.boxShadow = "";
 	document.getElementById("login").style.transform = "translate(100vw, -50%)";
 	login_form_enabled = false;
 }
@@ -53,8 +53,9 @@ function openAbout()
 	closeHome();
 	closeSignup();
 	document.getElementById("about-button").style.backgroundColor = "#c5c9cb";
-	document.getElementById("about-button").style.borderColor = "#bbbcbd";
-	document.getElementById("about").style.transition = "transform 0.25s ease-out";
+	document.getElementById("about-button").style.setProperty("--bs-border-color", "#bbbcbd");
+	document.getElementById("about").style.boxShadow = "rgba(0, 0, 0, 0.5) 0px 0px 0px 1000vmax";
+	document.getElementById("about").style.transition = "transform 0.25s ease-out, box-shadow 0.25s ease-out";
 	document.getElementById("about").style.transform = "translate(-50%, -50%)";
 }
 
@@ -65,10 +66,8 @@ function openHome()
 	closeLogin();
 	closeSignup();
 	document.getElementById("home-button").style.backgroundColor = "#c5c9cb";
-	document.getElementById("home-button").style.borderColor = "#bbbcbd";
-	document.getElementById("content").style.filter = "";
+	document.getElementById("home-button").style.setProperty("--bs-border-color", "#bbbcbd");
 	document.getElementById("content-body").style.pointerEvents = "";
-	document.getElementById("toolbar").style.filter = "";
 	document.getElementById("toolbar").style.pointerEvents = "";
 }
 
@@ -79,8 +78,9 @@ function openLogin()
 	closeHome();
 	closeSignup();
 	document.getElementById("login-button").style.backgroundColor = "#c5c9cb";
-	document.getElementById("login-button").style.borderColor = "#bbbcbd";
-	document.getElementById("login").style.transition = "transform 0.25s ease-out";
+	document.getElementById("login-button").style.setProperty("--bs-border-color", "#bbbcbd");
+	document.getElementById("login").style.boxShadow = "rgba(0, 0, 0, 0.5) 0px 0px 0px 1000vmax";
+	document.getElementById("login").style.transition = "transform 0.25s ease-out, box-shadow 0.25s ease-out";
 	document.getElementById("login").style.transform = "translate(-50%, -50%)";
 }
 
@@ -91,7 +91,8 @@ function openSignup()
 	closeHome();
 	closeLogin();
 	document.getElementById("login-button").style.backgroundColor = "#c5c9cb";
-	document.getElementById("login-button").style.borderColor = "#bbbcbd";
+	document.getElementById("login-button").style.setProperty("--bs-border-color", "#bbbcbd");
+	document.getElementById("login").style.boxShadow = "rgba(0, 0, 0, 0.5) 0px 0px 0px 1000vmax";
 	document.getElementById("signup").style.transition = "transform 0.25s ease-out";
 	document.getElementById("signup").style.transform = "translate(-50%, -50%)";
 }
